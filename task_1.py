7cb97a9eadb7dd3069102ce7eb4ae9aa2abde1fa
Denis Gubich <dm@gubich.pro>
2022-07-11 12:56:12 +0500

    Binary search for task 1

--- task_1.py
+++ task_1.py
@@ -1,4 +1,18 @@
 def task(array):
-    return array.index('0')
+    left = -1
+    right = len(array)
 
-print(task("111111111110000000000000000"))
+    while right - left > 1:
+        middle = (left + right) // 2
+
+        if int(array[middle]) == 1:
+            left = middle
+
+        else:
+            right = middle
+
+    return right
+
+
+print(task("1111111111100000000000000000"))
+
